12245829ceb6451c80bfa13a74ddc81906c284cc
Vladimir Tishchenko <695amr@mail.ru>
2021-02-03 00:12:01 +0300

    newCommit

---
+++ projects/epgu-chat/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  name: 'prod',
+  apiUrl: 'https://mc-gudom-dev.test.gosuslugi.ru',
+  wss: 'wss://gudom-uat.test.gosuslugi.ru/chats/ws/chat',
+  imageSize: 500,
+};
